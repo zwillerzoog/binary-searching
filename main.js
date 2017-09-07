fd127b75d51f212e522d6d0f1340c7f8eeb6cd44
daviddinnison <david.g.dinnison@gmail.com>
2017-09-07 15:04:22 -0400

    working findNumber

--- main.js
+++ main.js
@@ -66,6 +66,23 @@ function breadthInOrder(tree, values=[]) {
     return values;
 }
 
+function findNumber(array, guess) {
+    if(typeof array === 'string') {
+        array = array.split(' ')
+    }
+
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] === guess) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+const numbers = '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5 76 62';
+const alternateArray = [90, 28, 1, 700, 40, 23]
+
 let bt = new BinaryTree();
 
 let array = [];
@@ -74,9 +91,10 @@ bt.insert('3', '3');
 bt.insert('4', '4');
 bt.insert('5', '5');
 bt.insert('6', '6');
-bt.insert('0', '0')
+bt.insert('0', '0');
 bt.insert('1', '1');
 bt.insert('2', '2');
 
+console.log(findNumber(alternateArray, 23));
 // console.log(depthInOrder(bt, array))
-console.log(breadthInOrder(bt, array))
+// console.log(breadthInOrder(bt, array))
